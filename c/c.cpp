19cb5b0a0416591ef2052f0f4d70eb209c635a28
Veronika Gazdovskaya <veronika.gazdovskaya@gmail.com>
2021-09-13 23:17:28 +0300

    my first commit

---
+++ c/c.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    printf ("Input two numbers: ");
+    scanf ("%d %d", &a, &b);
+    printf ("%d %d\n", a + 1, b + 1);
+    return 0;
+}
